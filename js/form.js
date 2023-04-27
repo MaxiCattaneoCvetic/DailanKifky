7341b4546805803a41b4c954a1a687e1d7e34ebd
MaxiCattaneoCvetic <cveticmaxi97@gmail.com>
2023-04-26 22:54:05 -0300

    Update about-contact pages

---
+++ js/form.js
@@ -0,0 +1,19 @@
+//formulario 
+
+
+
+
+window.addEventListener("load", function (){
+    
+let formulario = document.querySelector("#contact_form");
+
+formulario.addEventListener('submit', function(event){
+    event.preventDefault();
+
+    
+
+})
+
+
+
+});
